d5d5032d3445ccd8602582631c8eea449857d064
musadhiek <musadhiek@gmail.com>
2020-11-07 12:46:07 +0530

    main user account files

---
+++ accounts/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from accounts import views
+
+
+urlpatterns = [
+    path('',views.home, name='home'),
+    path('login',views.login_request,name='login'),
+    path('logout_request',views.logout_request,name='logout'),
+    path('admin_page',views.adminpage,name='admin_page'),
+    path('adminusers',views.adminusers,name='adminusers'),
+    path('user_page', views.userpage,name='user_page'),
+    path('vendor_page',views.vendorpage,name='vendor_page'),
+    path('vendor_signup',views.vendor_signup,name='vendor_signup'),
+    path('add_vendor',views.add_vendor,name='add_vendor'),
+    path('user_signup',views.user_signup,name='user_signup'),
+    path('add_user', views.add_user,name='add_user'),
+    path('delete_user/<int:id>',views.delete_user,name='delete_user'),
+
+]
---
+++ accounts/views.py
@@ -0,0 +1,148 @@
+from django.shortcuts import render,redirect
+from django.contrib import auth
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+
+# Create your views here.
+def logout_request(request):
+
+    auth.logout(request)
+    return redirect(home)
+
+def login_request(request):
+    
+    if request.user.is_authenticated and not request.user.is_staff and not request.user.is_superuser:    
+        return redirect(userpage)
+    elif request.user.is_authenticated and request.user.is_staff and not request.user.is_superuser:    
+        return redirect(vendorpage)
+
+    elif request.user.is_authenticated and request.user.is_superuser:
+        return redirect(adminpage) 
+    
+    else:
+        if  request.method == 'POST':
+            username = request.POST['username']
+            password = request.POST['password']
+        
+            user = auth.authenticate(username=username,password=password)
+            if user is not None and not user.is_staff and not user.is_superuser:
+                auth.login(request,user)
+                return redirect(userpage)
+            
+            elif user is not None and user.is_staff and not user.is_superuser:
+                auth.login(request,user)
+                return redirect(vendorpage)
+
+            elif user is not None and user.is_superuser:
+                auth.login(request,user)
+                return redirect(adminpage)
+            else:        
+                return render(request,'login.html',{'warning':'incorrect username or password'})    
+
+    return render(request,'login.html')
+
+def home(request):
+    return render(request,'home.html')
+
+def adminpage(request):
+    if request.user.is_authenticated:
+        users = User.objects.filter(is_superuser=False,is_staff=True)
+        return render(request,'adminpage.html',{'users':users})
+    else:
+        return redirect(home)
+
+def adminusers(request):
+    if request.user.is_authenticated:
+        users = User.objects.filter(is_superuser=False,is_staff=False)
+        return render(request,'adminpage.html',{'users':users})
+    else:
+        return redirect(home)        
+def userpage(request):
+    if request.user.is_authenticated:
+        return render(request,'userpage.html')
+    else:
+        return redirect(home)
+
+def vendorpage(request):
+    if request.user.is_authenticated:
+        return render(request,'vendorpage.html')
+    else:
+        return redirect(home)
+def vendor_signup(request):
+    if request.method=='POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['password2']
+        if password == confirm_password:
+            if User.objects.filter(email=email).exists():
+                return render(request, 'vendor_signup.html',{'warning':'a user with this email already exists'})
+            else:
+                user =User.objects.create_user(password=password,email=email,username=username, is_staff=True,is_superuser=False)
+                user.save()
+                auth.login(request,user)
+                return render(request,'vendorpage.html')
+        else:
+             return render(request, 'vendor_signup.html',{'warning':'passwords do not match'})     
+        return(request,'vendor_signup.html')       
+    return render(request,'vendor_signup.html')
+
+def add_vendor(request):
+    if request.method=='POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['password2']
+        if password == confirm_password:
+            if User.objects.filter(email=email).exists():
+                return render(request, 'add_vendor.html',{'warning':'a user with this email already exists'})
+            else:
+                user =User.objects.create_user(username=username,email=email,password=password,is_staff=True,is_superuser=False)
+                user.save()
+                users = User.objects.filter(is_superuser=False)
+            return redirect(adminpage)
+        else:
+             return render(request, 'add_vendor.html',{'warning':'passwords do not match'})      
+    return render(request,'add_vendor.html')
+
+
+def delete_user(request,id):
+    user= User.objects.get(id=id)
+    user.delete()
+    return redirect(adminpage)
+
+def user_signup(request):
+    if request.method=='POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['password2']
+        if password == confirm_password:
+            if User.objects.filter(email=email).exists():
+                return render(request, 'user_signup.html',{'warning':'a user with this email already exists'})
+            else:
+                user =User.objects.create_user(username=username,email=email,password=password,is_staff=False,is_superuser=False)
+                user.save()
+                auth.login(request,user)
+                return render(request,'userpage.html')
+        else:
+             return render(request, 'user_signup.html',{'warning':'passwords do not match'})     
+        return(request,'user_signup.html')       
+    return render(request,'user_signup.html')
+
+def add_user(request):
+    if request.method=='POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['password2']
+        if password == confirm_password:
+            if User.objects.filter(email=email).exists():
+                return render(request, 'add_user.html',{'warning':'a user with this email already exists'})
+            else:
+                user =User.objects.create_user(username=username,email=email,password=password,is_staff=False,is_superuser=False)
+                user.save()
+                return redirect(adminpage)
+        else:
+             return render(request, 'add_user.html',{'warning':'passwords do not match'})      
+    return render(request,'add_user.html')
